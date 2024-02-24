fc81387c4e7d4f83022aacab77aa52ccc3a3274e
alherd havartsou <alherd.havartsou@gmail.com>
2024-02-24 21:49:17 +0100

    m

---
+++ scripts/main/main.js
@@ -0,0 +1,15 @@
+
+
+//const width = window.innerWidth;
+//let height = Telegram.WebApp.viewportStableHeight;
+//let app = new PIXI.Application({ width: width * 0.9, height: height});
+
+const moneyElem = document.getElementById("money");
+
+console.log(Telegram.WebApp.initData);
+// ToDo: getBoons amount
+let boonAmount = 0.01;
+// ToDo: getBoons per second
+let boonPerSecond = 0.0001;
+// Timer to increase boonAmount
+setInterval(() => {boonAmount += boonPerSecond; moneyElem.innerHTML = boonAmount.toFixed(4)}, 1000);
