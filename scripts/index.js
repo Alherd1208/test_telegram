46089a2aa07709a4f1c39e8751bf08bb44257795
hovort <alherd.havartsou@gmail.com>
2024-03-02 23:24:32 +0000

    m

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,52 @@
- //Init TWA
+//Init TWA
 Telegram.WebApp.ready();
 // Event occurs whenever theme settings are changed in the user's Telegram app (including switching to night mode).
-Telegram.WebApp.onEvent('themeChanged', function() {
+Telegram.WebApp.onEvent('themeChanged', function ()
+{
     document.documentElement.className = Telegram.WebApp.colorScheme;
 });
 // Show main button
 Telegram.WebApp.MainButton.setParams({
     text: 'Lunar Bananas V1'
 });
-Telegram.WebApp.MainButton.onClick(function () {
+Telegram.WebApp.MainButton.onClick(function ()
+{
     Telegram.WebApp.showAlert('Main Button was clicked')
-});	
+});
 Telegram.WebApp.MainButton.show();
 // Function to call showPopup API
-function showPopup() {
+function showPopup()
+{
     Telegram.WebApp.showPopup({
         title: 'Title',
         message: 'Some message',
         buttons: [
-            {id: 'link', type: 'default', text: 'Open ton.org'},
-            {type: 'cancel'},
+            { id: 'link', type: 'default', text: 'Open ton.org' },
+            { type: 'cancel' },
         ]
-    }, function(btn) {
-        if (btn === 'link') {
+    }, function (btn)
+    {
+        if (btn === 'link')
+        {
             Telegram.WebApp.openLink('https://ton.org/');
         }
     });
 };
 // Function to toggle main TWA button
-function toggleMainButton() {
-    if (Telegram.WebApp.MainButton.isVisible) {
+function toggleMainButton()
+{
+    if (Telegram.WebApp.MainButton.isVisible)
+    {
         Telegram.WebApp.MainButton.hide();
-    } else {
+    } else
+    {
         Telegram.WebApp.MainButton.show();
     }
 };
-function setViewportData() {
+function setViewportData()
+{
     var sizeEl = document.getElementById('viewport-params-size');
-    sizeEl.innerText = 'width: ' + window.innerWidth + ' x ' + 
+    sizeEl.innerText = 'width: ' + window.innerWidth + ' x ' +
         'height: ' + Telegram.WebApp.viewportStableHeight;
     var expandEl = document.querySelector('#viewport-params-expand');
     expandEl.innerText = 'Is Expanded: ' + (Telegram.WebApp.isExpanded ? 'true' : 'false');
@@ -45,28 +54,31 @@ function setViewportData() {
 Telegram.WebApp.setHeaderColor('secondary_bg_color');
 setViewportData();
 Telegram.WebApp.onEvent('viewportChanged', setViewportData);
-Telegram.WebApp.onEvent('themeChanged', function() {
+Telegram.WebApp.onEvent('themeChanged', function ()
+{
     document.body.setAttribute('style', '--bg-color:' + Telegram.WebApp.backgroundColor);
 });
 
-        // <!-- <button onclick="Telegram.WebApp.showAlert('Hello World!');">Launch Alert</button>
-        // <button onclick="showPopup();">Launch Popup</button> -->
+Telegram.WebApp.expand();
 
-        // <!-- <h1>Links</h1>
-        // <ul>
-        //     <li>
-        //         <a href="javascript:Telegram.WebApp.openTelegramLink('https://t.me/trendingapps');">Open link within Telegram 3</a>
-        //     </li>
-        //     <li>
-        //         <a href="javascript:Telegram.WebApp.openLink('https://ton.org/');">Open link in external browser</a>
-        //     </li>
-        //     <li>
-        //         <a href="javascript:Telegram.WebApp.openLink('https://telegra.ph/api',{try_instant_view:true});">Open link inside Telegram webview</a>
-        //     </li>
-        // </ul> -->
+// <!-- <button onclick="Telegram.WebApp.showAlert('Hello World!');">Launch Alert</button>
+// <button onclick="showPopup();">Launch Popup</button> -->
 
-        // <!-- <h1>Buttons</h1>
-        // <button onclick="Telegram.WebApp.expand();">Expand Webview</button>
-        // <button onclick="toggleMainButton();">Toggle Main Button</button> -->
+// <!-- <h1>Links</h1>
+// <ul>
+//     <li>
+//         <a href="javascript:Telegram.WebApp.openTelegramLink('https://t.me/trendingapps');">Open link within Telegram 3</a>
+//     </li>
+//     <li>
+//         <a href="javascript:Telegram.WebApp.openLink('https://ton.org/');">Open link in external browser</a>
+//     </li>
+//     <li>
+//         <a href="javascript:Telegram.WebApp.openLink('https://telegra.ph/api',{try_instant_view:true});">Open link inside Telegram webview</a>
+//     </li>
+// </ul> -->
+
+// <!-- <h1>Buttons</h1>
+// <button onclick="Telegram.WebApp.expand();">Expand Webview</button>
+// <button onclick="toggleMainButton();">Toggle Main Button</button> -->
 
 
--- scripts/main/main.js
+++ scripts/main/main.js
@@ -4,15 +4,15 @@
 //let height = Telegram.WebApp.viewportStableHeight;
 //let app = new PIXI.Application({ width: width * 0.9, height: height});
 
-const moneyElem = document.getElementById("money");
+// const moneyElem = document.getElementById("money");
 
-console.log(Telegram.WebApp.initData);
-console.log("qwe");
-moneyElem.innerHTML = Telegram.WebApp.initData;
+// console.log(Telegram.WebApp.initData);
+// console.log("qwe");
+// moneyElem.innerHTML = Telegram.WebApp.initData;
 // ToDo: getBoons amount
 //let boonAmount = 0.01;
 // ToDo: getBoons per second
 //let boonPerSecond = 0.0001;
 // Timer to increase boonAmount
-setInterval(() => {moneyElem.innerHTML = Telegram.WebApp.initData;}, 1000);
+//setInterval(() => {moneyElem.innerHTML = Telegram.WebApp.initData;}, 1000);
 //setInterval(() => {boonAmount += boonPerSecond; moneyElem.innerHTML = boonAmount.toFixed(4)}, 1000);
